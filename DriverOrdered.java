c217086aa641b15114fc4a3d4ee5e40f8959e0ab
meredith <meredith>
2017-11-26 20:52:30 -0500

    Removed file

--- DriverOrdered.java
+++
@@ -1,8 +0,0 @@
-public class DriverOrdered{
-    public static void main(String[] args){
-	OrderedSuperArray a = new OrderedSuperArray();
-	System.out.println(a);
-	OrderedSuperArray test = a.slice(4,7);
-	System.out.println(test);
-    }
-}
