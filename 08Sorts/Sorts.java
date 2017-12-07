5117273e445105187b8d99354af213bbe20c1386
Meredith <mhu2@stuy.edu>
2017-12-07 14:21:17 -0500

    added insertionSort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -9,4 +9,21 @@ public class Sorts{
 	    min=Math.min(data[i],data[i+1]);
 	}
     }
+
+    public static void insertionSort(int[] ary){
+	int index=1;
+	while(index<ary.length){
+	    int j=index;
+	    while(j>0 && ary[j]>ary[j-1]){
+		swap(ary,j,j-1);
+		j=j-1;
+	    }
+	    index++;
+	}
+    }
+    public static void swap(int[] ary, int a, int b){
+	int temp=ary[a];
+	ary[a]=ary[b];
+	ary[b]=temp;
+    }
 }
