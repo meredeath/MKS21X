be91b2c1fd127d6d764eb51bcc64a7d65d799855
Meredith <mhu2@stuy.edu>
2017-12-07 14:33:14 -0500

    added swap, isSorted, and toString and tested with a main, insertion sort does not work (sorts backwards)

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,4 +1,18 @@
+import java.util.Arrays.*;
 public class Sorts{
+    public static void main(String[] args){
+	int[] anArray = new int[10];
+	for(int i=0;i<anArray.length;i++){
+	    anArray[i]=(int)(Math.random()*100);
+	}
+	System.out.println(isSorted(anArray));
+	System.out.println(name());
+	System.out.println(java.util.Arrays.toString(anArray));
+	insertionSort(anArray);
+	System.out.println(java.util.Arrays.toString(anArray));
+	System.out.println(isSorted(anArray));
+    }
+    
     public static String name(){
 	return "09.Hu.Meredith";
     }
@@ -22,8 +36,17 @@ public class Sorts{
 	}
     }
     public static void swap(int[] ary, int a, int b){
-	int temp=ary[a];
-	ary[a]=ary[b];
-	ary[b]=temp;
+	int temp=ary[b];
+	ary[b]=ary[a];
+	ary[a]=temp;
+    }
+
+    public static boolean isSorted(int[] ary){
+	for(int i=0;i<ary.length-1;i++){
+	    if(ary[i]>ary[i+1]){
+		return false;
+	    }
+	}
+	return true;
     }
 }
