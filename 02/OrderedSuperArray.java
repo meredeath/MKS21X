58d902678af46337921a1e6b5f34be099c716df7
Meredith Hu <humeiyin@gmail.com>
2018-01-07 18:27:43 -0500

    whoops didn't git properly but yes finally finally fixed binary search

--- 02/OrderedSuperArray.java
+++ 02/OrderedSuperArray.java
@@ -14,51 +14,11 @@ public class OrderedSuperArray extends SuperArray{
 	add(value);
     }
 
-    /*
-    public boolean add(String value){
-	if (size == 0){
-	    return false;
-	}
-	int lowerBound = 0;
-	int upperBound = size-1;
-	int curIn = 0;
-	while (true) {
-	    curIn = (upperBound + lowerBound) / 2;
-	    if (data[curIn].compareTo(value)==0) {
-		super.add(curIn,value);
-		return true;
-	    } else if (data[curIn].compareTo(value)<0) {
-		lowerBound = curIn + 1; // its in the upper
-		if (lowerBound > upperBound){
-		    super.add(curIn + 1,value);
-		    return true;
-	    } else {
-		upperBound = curIn - 1; // its in the lower
-		if (lowerBound > upperBound){
-		    super.add(curIn,value);
-		    return true;
-		}
-		}
-	    }
-	}
-    }
-    */
     public boolean add(String value){
 	if(size()==0){
 	    super.add(value);
 	    return true;
 	}
-	/*
-	if(size()==1){
-	    if(data()[0].compareTo(value)<0){
-		super.add(value);
-		return true;
-	    }else{
-		super.add(0,value);
-		return true;
-	    }
-	}
-	*/
 	int high = size();
 	int low = 0;
 	int guess = 0;
@@ -76,31 +36,4 @@ public class OrderedSuperArray extends SuperArray{
 	super.add(low,value);
 	return true;
     }
-
-    /*
-    public OrderedSuperArray slice(int start, int end){
-	OrderedSuperArray result = new OrderedSuperArray(end-start);
-	int index=0;
-	for(int i=start;i<end;i++){
-	    result.set(index,get(i));
-	    index++;
-	}
-	return result;
-    }
-
-
-    public boolean add(String value){
-	for(int i=0;i<size;i++){
-	    if(this.get(i)==null){
-		continue;
-	    }
-	    if(this.get(i).compareTo(value)>=0){
-		super.add(i,value);
-		return true;
-	    }
-	}
-	super.add(value);
-	return true;
-    }
-    */
 }
